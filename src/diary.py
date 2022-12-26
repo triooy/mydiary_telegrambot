9f490da84e7cad51f4bfdba379ca49ecea90c13b
David Hustadt <dh@ai.associates>
2022-12-26 02:29:04 +0100

    Bugfix, cast column entry to string

--- src/diary.py
+++ src/diary.py
@@ -16,6 +16,8 @@ def create_diary_entry(text):
     # create a new diary entry
     # get current date
     date = datetime.now()
+    time_ = datetime.now().strftime('-- %H:%M --')
+    text = time_ + "\n" + str(text) + "\n"
     df = pd.DataFrame({'date': [date], 'entry': [text], 'images': [[]]})
     logger.info(f"New diary entry created: {df}")
     return df
@@ -26,6 +28,7 @@ def get_diary(config):
     df = pd.read_csv(config.get('diary_csv'))
     df['images'] = df['images'].apply(ast.literal_eval)
     df['date'] = pd.to_datetime(df['date'])
+    df['entry'] = df['entry'].astype(str)
     return df
 
 def get_entry_by_date(date, config):
@@ -54,7 +57,6 @@ def process_new_text(update: Update, context: CallbackContext, config):
             # if there is already an entry for today, append the new text to the existing entry
             # strftime() is used to convert the datetime object to a string
             logger.info(f"Entry for today already exists: {diary_today}")
-            today = datetime.now().strftime('%d.%m.%Y %H:%M')
             diary_today['entry'] = diary_today['entry'].values[0] + f"\n\n{today}\n" + df['entry'].values[0]
             diary_today['images'] = [diary_today['images'].values[0] + df['images'].values[0]]
             df = diary_today
